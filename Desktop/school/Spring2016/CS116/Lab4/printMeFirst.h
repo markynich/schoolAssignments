b493cdb590ffdef43c48d331d18133e0bb1a6576
Mark-Nicholas Magsakay <mark.magsakay@gmail.com>
2020-01-15 14:31:53 -0800

    Adding object-oriented hw from 2016

---
+++ Desktop/school/Spring2016/CS116/Lab4/printMeFirst.h
@@ -0,0 +1,11 @@
+#ifndef PRINTMEFIRST_H
+#define PRINTMEFIRST_H
+#include <iostream>
+#include <ctime>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+void printMeFirst(string name, string courseInfo);
+
+#endif 
---
+++ Desktop/school/Spring2016/CS116/Lab4_ver2/printMeFirst.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+/**
+Purpose:
+Print out the programmer’s information such as name, class information
+and date/time the program is run
+@author Ron Sha
+@version 1.0 1/27/2017
+@param name - the name of the programmer
+@param courseInfo - the name of the course
+@return - none
+*/
+void printMeFirst(string name, string courseInfo)
+{
+	cout <<" Program written by: "<< name << endl; // put your name here
+	cout <<" Course info: "<< courseInfo << endl;
+	time_t now = time(0); // current date/time based on current system
+	char* dt = ctime(&now); // convert now to string for
+	cout << " Date: " << dt << endl;
+}
